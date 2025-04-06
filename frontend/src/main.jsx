60b4cabfefc4291d4f65386033a568341320493c
Winston Malcolm <programmermalx27@gmail.com>
2025-04-05 21:34:41 -0500

    change router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import "./index.css";
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import {  HashRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { TokenContextProvider } from './contexts/tokenContextProvider.jsx';
@@ -10,11 +10,11 @@ import { TokenContextProvider } from './contexts/tokenContextProvider.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     
-      <BrowserRouter>
+      <HashRouter>
       <TokenContextProvider>
         <App />
       </TokenContextProvider>
-      </BrowserRouter>
+      </HashRouter>
       <ToastContainer theme='Dark' />
   </StrictMode>,
 )
