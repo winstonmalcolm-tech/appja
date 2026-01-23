9cfe94d14130b72f1d104434d66dc313eb9ced63
Winston Malcolm <winston.malcolm2016@gmail.com>
2026-01-23 13:36:35 -0500

    fix database connection

--- backend/config/db_config.js
+++ backend/config/db_config.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql2/promise");
 
 
-const url = `mysql://avnadmin:${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT}/${process.env.MYSQL_DB}?ssl-mode=REQUIRED`;
+//const url = `mysql://avnadmin:${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT}/${process.env.MYSQL_DB}?ssl-mode=REQUIRED`;
 
-const connection = mysql.createPool(url)
-// const connection = mysql.createPool({
-//     host: process.env.MYSQL_HOST,
-//     user: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.MYSQL_DB
-// });
+//const connection = mysql.createPool(url)
+const connection = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
+});
 
 module.exports = connection;
 
